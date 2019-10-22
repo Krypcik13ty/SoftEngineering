13ce37027337f355641fe5907818acc51f710ad6
dasad33 <dasad33_79@o2.pl>
2019-10-22 16:12:36 +0200

    szablon strony dla recznego wyboru recznego planu

--- SoftEngineering/Controllers/HomeController.cs
+++ SoftEngineering/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace SoftEngineering.Controllers
         {
             return View();
         }
+
+        public ActionResult ManualTimetable()
+        {
+            return View();
+        }
     }
 }

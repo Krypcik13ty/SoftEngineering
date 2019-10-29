49ca0d1bb2b959c60aa831972c3b2386e7c718f0
DonKichot <30420465+DonKichot@users.noreply.github.com>
2019-10-29 05:01:39 +0100

    Updating packages to the newer versions and some minor tweaks.

--- SoftEngineering/Global.asax.cs
+++ SoftEngineering/Global.asax.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,7 +8,7 @@ using System.Web.Routing;
 
 namespace SoftEngineering
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {

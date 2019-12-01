0ada1adc30341dbd303572ab8ef27747e9ece218
Michal <stasiewicz1997@gmail.com>
2019-12-01 12:43:42 +0100

    zrobiłem przycisk do funkcji administratora oraz dodalem komentarze do scryptu kalendarza

--- SoftEngineering/Scripts/calendarscipt.js
+++ SoftEngineering/Scripts/calendarscipt.js
@@ -32,23 +32,23 @@ function showCalendar(month, year) {
 
     let firstDay = (new Date(year, month)).getDay();
 
-    tbl = document.getElementById("calendar-body"); // body of the calendar
+    tbl = document.getElementById("calendar-body"); // zawartoœæ kalendarza
 
-    // clearing all previous cells
+    // czyszczenie poprzednich komórek
     tbl.innerHTML = "";
 
-    // filing data about month and in the page via DOM.
+    // wpisywanie daty
     monthAndYear.innerHTML = months[month] + " " + year;
     selectYear.value = year;
     selectMonth.value = month;
 
-    // creating all cells
+    // tworzenie bloków z dat¹
     let date = 1;
     for (let i = 0; i < 7; i++) {
-        // creates a table row
+        // tworzenie rzêdów w tabeli kalendarza
         let row = document.createElement("tr");
 
-        //creating individual cells, filing them up with data.
+        //tworzenie pojedyñczych komórek, wype³nianie ich dat¹
         for (let j = 0; j < 7; j++) {
             if (i === 0 && j < firstDay) {
                 cell = document.createElement("td");
@@ -65,7 +65,7 @@ function showCalendar(month, year) {
                 cellText = document.createTextNode(date);
                 if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
                     cell.classList.add("bg-info");
-                } // color today's date
+                } // zaznaczenie kolorem dzisiejszej daty
                 cell.appendChild(cellText);
                 row.appendChild(cell);
                 date++;
@@ -74,13 +74,13 @@ function showCalendar(month, year) {
 
         }
 
-        tbl.appendChild(row); // appending each row into calendar body.
+        tbl.appendChild(row); // do³¹czanie ka¿dego wiersza do kalendarza
     }
 
 }
 
 
-// check how many days in a month code from https://dzone.com/articles/determining-number-days-month
+// Funkcja sprawdzaj¹ca ile jest dni w miesi¹cu link do skryptu  https://dzone.com/articles/determining-number-days-month
 function daysInMonth(iMonth, iYear) {
     return 32 - new Date(iYear, iMonth, 32).getDate();
 }

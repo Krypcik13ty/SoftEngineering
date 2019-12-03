adb2f7b922a8a3834121f8b5f3128f31632b986c
dasad33 <dasad33_79@o2.pl>
2019-12-03 15:55:32 +0100

    DB connection function

--- SoftEngineering/Controllers/TemporaryController.cs
+++ SoftEngineering/Controllers/TemporaryController.cs
@@ -5,13 +5,15 @@ using System.Web;
 using System.Web.Mvc;
 using System.Net;
 using System.Net.Mail;
+using System.Data.SqlClient;
+using System.Windows;
 
 namespace SoftEngineering.Controllers
 {
     public class TemporaryController : Controller
     {
         // GET: Temporary
-        public ActionResult EmailSender()
+        public ActionResult TemporaryView()
         {
             //sendMail();
             return View();
@@ -24,15 +26,52 @@ namespace SoftEngineering.Controllers
             client.Credentials = new NetworkCredential("21edqcds@gmail.com", "P@ssw0rd_");
             client.EnableSsl = true;
             MailAddress from = new MailAddress("21edqcds@gmail.com");
+            try
+            {
+                MailAddress to = new MailAddress("radziomenek@gmail.com");
+                MailMessage message = new MailMessage(from, to);
 
-            MailAddress to = new MailAddress("dasad44_79@o2.pl");
-            MailMessage message = new MailMessage(from, to);
-            message.Body = "Kghkjhhjdjd";
-            message.Subject = "Nfsafam sfaIe ! ;*";
-            client.Send(message);
+                message.Body = "Kghkjhhjdjd";
+                message.Subject = "Nfsafam sfaIe ! ;*";
+                client.Send(message);
+                client.Dispose();
+                MessageBox.Show("Wyslano maila");
+            }
+            catch (FormatException e)
+            {
+                MessageBox.Show("Zły format maila");
+            }
+            catch (Exception e)    //other exceptions
+            {
+                MessageBox.Show("Błąd podczas wysyłania miaila");
+            }
+            return View("TemporaryView");
+        }
 
-            client.Dispose();
-            return View("EmailSender");
+        public ActionResult connectToDB()
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            string sqlcommand = "SELECT * from Accounts";
+
+            connetionString = "Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password";
+
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sqlcommand, connection);
+                command.ExecuteNonQuery();
+                command.Dispose();
+                connection.Close();
+                MessageBox.Show("Połączono");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Błąd połączenia");
+            }
+            return View("TemporaryView");
         }
     }
 }

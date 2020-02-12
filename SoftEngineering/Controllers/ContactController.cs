35deaaf2bf0549d4866a17d09b745f8ab4ac21d6
dasad44 <dasad33_79@o2.pl>
2020-02-12 20:20:51 +0100

    MailSender frontend fixed

--- SoftEngineering/Controllers/ContactController.cs
+++ SoftEngineering/Controllers/ContactController.cs
@@ -2,8 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using System.Windows;
 
 namespace SoftEngineering.Controllers
 {
@@ -16,9 +19,14 @@ namespace SoftEngineering.Controllers
         }
 
         [HttpPost]
-        public ActionResult ContactMail()
+        public ActionResult ContactMail(MailSender model)
         {
             MailSender mailsender = new MailSender();
+            mailsender.mailadress = model.mailadress;
+            mailsender.mailtext = model.mailtext;
+            mailsender.firstname = model.firstname;
+            mailsender.lastname = model.lastname;
+            mailsender.mailsubject = model.mailsubject;
             mailsender.SendMail();
             return View("Contact");
         }

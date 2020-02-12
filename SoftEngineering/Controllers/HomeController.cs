65d1b4d33fa4a53e680d250e80e6066dc36379e9
Michal <stasiewicz1997@gmail.com>
2020-02-12 11:08:53 +0100

    GroupChoice created

--- SoftEngineering/Controllers/HomeController.cs
+++ SoftEngineering/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace SoftEngineering.Controllers
     public class HomeController : Controller
     {
         string subjectquery = "SELECT Subject FROM SUBJECTS_DICTIONARY ";
+        string grquery = "SELECT GroupName FROM groups ";
         [HttpGet]
 
         // GET: Home
@@ -19,12 +20,14 @@ namespace SoftEngineering.Controllers
         public ActionResult ManualTimetable()
         {
             List<string> subjectList = new List<string>();
+            List<string> grouplist = new List<string>();
             DBConnection dbconnection = new DBConnection();
             dbconnection.ConnectionToList(subjectquery, subjectList);
+            dbconnection.ConnectionToList(grquery, grouplist);
             ViewData["subjects"] = subjectList;
+            ViewData["groups"] = grouplist;
             return View();
         }
-
         public ActionResult LogOut()
         {
             return View();

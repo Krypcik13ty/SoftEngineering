e064d4a78d01787b46cf38b766b5b3a122e632f9
Michał <michal.wojdala@o2.pl>
2020-02-26 14:10:00 +0100

    Sub_Edit

--- SoftEngineering/Controllers/AdminPanelController.cs
+++ SoftEngineering/Controllers/AdminPanelController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Windows;
 using SoftEngineering.Models;
 
 namespace SoftEngineering.Controllers
@@ -13,16 +14,20 @@ namespace SoftEngineering.Controllers
         DBConnection dbconnection = new DBConnection();
         public ActionResult AdminPanel()
         {
-            string lecturerquery = "SELECT CONCAT(Name, '" + " ', Surname) AS lecturer FROM lecturers";
+            string lecturerquery = "SELECT Login AS lecturer FROM lecturers";
             string subjectQuery = "SELECT Subject FROM subjects_dictionary";
             string classesQuery = "SELECT ClassNR FROM classes";
+            string hoursQuery = "SELECT CONVERT(hours_dictionary.Hour, char) as hour FROM hours_dictionary";
             List<string> lecturersList = new List<string>();
+            List<string> hoursList = new List<string>();
             List<string> subjectList = new List<string>();
             List<string> classesList = new List<string>();
             dbconnection.ConnectionToList(lecturerquery, lecturersList);
+            dbconnection.ConnectionToList(hoursQuery, hoursList);
             dbconnection.ConnectionToList(subjectQuery, subjectList);
             dbconnection.ConnectionToList(classesQuery, classesList);
             ViewData["lecturers"] = lecturersList;
+            ViewData["hours"] = hoursList;
             ViewData["subjects_dictionary"] = subjectList;
             ViewData["classes"] = classesList;
             return View();
@@ -35,24 +40,49 @@ namespace SoftEngineering.Controllers
             DBConnection dbconnection = new DBConnection();
             dbconnection.ExecuteQuery(subAdd);
 
-            string lecturerquery = "SELECT CONCAT(Name, '" + " ', Surname) AS lecturer FROM lecturers";
+            string lecturerquery = "SELECT Login AS lecturer FROM lecturers";
+            string subjectQuery = "SELECT Subject FROM subjects_dictionary";
+            string classesQuery = "SELECT ClassNR FROM classes";
+            string hoursQuery = "SELECT CONVERT(hours_dictionary.Hour, char) as hour FROM hours_dictionary";
             List<string> lecturersList = new List<string>();
+            List<string> hoursList = new List<string>();
+            List<string> subjectList = new List<string>();
+            List<string> classesList = new List<string>();
             dbconnection.ConnectionToList(lecturerquery, lecturersList);
+            dbconnection.ConnectionToList(hoursQuery, hoursList);
+            dbconnection.ConnectionToList(subjectQuery, subjectList);
+            dbconnection.ConnectionToList(classesQuery, classesList);
             ViewData["lecturers"] = lecturersList;
+            ViewData["hours"] = hoursList;
+            ViewData["subjects_dictionary"] = subjectList;
+            ViewData["classes"] = classesList;
             return View("AdminPanel");
         }
 
-
-        public ActionResult SubjectEdit(SubjectAdder model)
+        [HttpPost]
+        public ActionResult SubjectEdit(SubEdit model)
         {
-            string subAdd = "INSERT INTO subjects_dictionary(Subject) VALUES ('" + model.NewSubject + "');";
+            string subEdit = "UPDATE schedule SET Class=('" + model.Change4 + "'),Lecturer=('" + model.Change1 + "'),DateId=('" + model.Change3 + "')" +
+                "Where Class=('" + model.Ch4 + "') AND Lecturer=('" + model.Ch1 + "') AND DateId=('" + model.Ch3 + "');";
             DBConnection dbconnection = new DBConnection();
-            dbconnection.ExecuteQuery(subAdd);
+            dbconnection.ExecuteQuery(subEdit);
 
-            string lecturerquery = "SELECT CONCAT(Name, '" + " ', Surname) AS lecturer FROM lecturers";
+            string lecturerquery = "SELECT Login AS lecturer FROM lecturers";
+            string subjectQuery = "SELECT Subject FROM subjects_dictionary";
+            string classesQuery = "SELECT ClassNR FROM classes";
+            string hoursQuery = "SELECT CONVERT(hours_dictionary.Hour, char) as hour FROM hours_dictionary";
             List<string> lecturersList = new List<string>();
+            List<string> hoursList = new List<string>();
+            List<string> subjectList = new List<string>();
+            List<string> classesList = new List<string>();
             dbconnection.ConnectionToList(lecturerquery, lecturersList);
+            dbconnection.ConnectionToList(hoursQuery, hoursList);
+            dbconnection.ConnectionToList(subjectQuery, subjectList);
+            dbconnection.ConnectionToList(classesQuery, classesList);
             ViewData["lecturers"] = lecturersList;
+            ViewData["hours"] = hoursList;
+            ViewData["subjects_dictionary"] = subjectList;
+            ViewData["classes"] = classesList;
             return View("AdminPanel");
         }
 
---
+++ SoftEngineering/Models/SubEdit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SoftEngineering.Models
+{
+    public class SubEdit
+    {
+        [Required]
+        public string Change1 { get; set; }
+        [Required]
+        public string Change2 { get; set; }
+        [Required]
+        public string Change3 { get; set; }
+        [Required]
+        public string Change4 { get; set; }
+        [Required]
+        public string Change5 { get; set; }
+        [Required]
+        public string Ch1 { get; set; }
+        [Required]
+        public string Ch2 { get; set; }
+        [Required]
+        public string Ch3 { get; set; }
+        [Required]
+        public string Ch4 { get; set; }
+        [Required]
+        public string Ch5 { get; set; }
+    }
+    }
